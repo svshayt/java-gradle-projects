61c9483200630f1fa376465743c2247f55317b40
Vasily Shaytan <svshayt@gmail.com>
2023-12-21 21:27:01 +0300

    Add functional interfaces

---
+++ learning/modern-java-recipes/src/main/java/com/svshayt/basics/MyChildInterface.java
@@ -0,0 +1,8 @@
+package com.svshayt.basics;
+
+// После расширения функциональный интерфейс перестает
+//быть функциональным
+//@FunctionalInterface - не работает
+public interface MyChildInterface extends MyInterface {
+    int anotherMethod(); // Дополнительный абстрактный метод
+}
