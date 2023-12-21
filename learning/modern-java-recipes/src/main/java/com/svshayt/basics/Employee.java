694a6a8067d0bd7490c4994df1f7f68d4cf8b794
Vasily Shaytan <svshayt@gmail.com>
2023-12-21 22:32:58 +0300

    Add interfaces with default and static methods

---
+++ learning/modern-java-recipes/src/main/java/com/svshayt/basics/Employee.java
@@ -0,0 +1,13 @@
+package com.svshayt.basics;
+
+public interface Employee {
+    String getFirst();
+
+    String getLast();
+
+    void convertCaffeineToCodeForMoney();
+
+    default String getName() { // Метод по умолчанию, имеющий реализацию
+        return String.format("%s %s", getFirst(), getLast());
+    }
+}
