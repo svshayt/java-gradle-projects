2e2b3f0cf5709947cb2146ccac67c8a1700e31fa
Vasily Shaytan <svshayt@gmail.com>
2024-01-08 09:16:44 +0300

    Create a stream with comparator and sort demo

--- learning/modern-java-recipes/src/main/java/com/svshayt/comparator_collector/ComparatorSortDemo.java
+++ learning/modern-java-recipes/src/main/java/com/svshayt/comparator_collector/ComparatorSortDemo.java
@@ -2,8 +2,10 @@ package com.svshayt.comparator_collector;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class ComparatorSortDemo {
     private List<String> sampleStrings =
@@ -13,6 +15,9 @@ public class ComparatorSortDemo {
         var demo = new ComparatorSortDemo();
         System.out.println(demo.defaultSort());
         System.out.println(demo.defaultSortUsingStreams());
+
+        System.out.println(demo.lengthSortUsingSorted());
+        System.out.println(demo.lengthSortUsingComparator());
     }
 
     // Сортировка строк в лексикографическом порядке
@@ -25,6 +30,20 @@ public class ComparatorSortDemo {
     public List<String> defaultSortUsingStreams() {
         return sampleStrings.stream()
                 .sorted() // Сортировка по умолчанию, принятая в Java 8 и последующих версиях
-                .collect(Collectors.toList());
+                .collect(toList());
+    }
+
+    // Сортировка строк по длине
+    public List<String> lengthSortUsingSorted() {
+        return sampleStrings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length()) // Лямбда-выражение в качестве компаратора для сортировки по длине
+                .collect(toList());
+    }
+
+    // Сортировка строк по длине
+    public List<String> lengthSortUsingComparator() {
+        return sampleStrings.stream()
+                .sorted(Comparator.comparingInt(String::length)) // Метод comparingInt в качестве компаратора
+                .collect(toList());
     }
 }
